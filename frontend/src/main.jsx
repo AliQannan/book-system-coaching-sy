4f9fa424528af36ab63b77a093c9685f14a03fe5
AliQannan <aliqannan2003f@gmail.com>
2025-10-24 12:12:38 +0300

    aliqannan

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ClerkProvider } from "@clerk/clerk-react";
 import AppContextProvider from "./context/Context.jsx";
 import App from "./App.jsx";
 import "./index.css";
 
-const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY || "pk_test_ZmFzdC1mcm9nLTYyLmNsZXJrLmFjY291bnRzLmRldiQ";
 
 if (!clerkPublishableKey) {
   throw new Error("Missing Clerk Publishable Key! Please set VITE_CLERK_PUBLISHABLE_KEY in your .env file");
@@ -15,12 +13,12 @@ if (!clerkPublishableKey) {
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPublishableKey}>
+
       <BrowserRouter>
         <AppContextProvider>
           <App />
         </AppContextProvider>
       </BrowserRouter>
-    </ClerkProvider>
+
   </React.StrictMode>
 );
